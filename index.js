942c932b3563ebb8aa2daf0d0771f96db046fe0b
Blake Rossdeutcher <blakerossdeutcher@gmail.com>
2023-07-05 13:48:02 -0600

    questions 4 & 5

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
+// 1
 const coffeeMenu = require(`./coffee_data.js`);
 
 console.table(coffeeMenu);
 
 console.log();
 
+// 2
 const drinkArray = coffeeMenu.map((item) => item.name);
 
 console.log(drinkArray);
 
+// 3
 console.log(coffeeMenu.filter((item) => item.price <=5));
+
+// 4
+console.log(coffeeMenu.filter((item) => item.price % 2 === 0));
+
+// 5
+const orderTotal = (costTotal, drink) => {
+  return costTotal += drink.price;
+}
+console.log(coffeeMenu.reduce(orderTotal, 0));
