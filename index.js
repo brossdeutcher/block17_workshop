e6b7c7de87e1846504963d5814daea7e2b8aa5eb
David Arcibar <davidaarcibar@gmail.com>
2023-07-05 14:39:09 -0500

    2 and 3 are done

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const coffeeMenu = require(`./coffee_data.js`);
 console.table(coffeeMenu);
 
 console.log();
+
+const drinkArray = coffeeMenu.map((item) => item.name);
+
+console.log(drinkArray);
+
+console.log(coffeeMenu.filter((item) => item.price <=5));
